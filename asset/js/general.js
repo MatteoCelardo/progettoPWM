2a2826b92ea98a18d6ee4dcae4343cc7ef545b4f
Teo <matteo.celardo.social@gmail.com>
2022-07-15 13:27:27 +0200

    modifica del nome da index.js a general.js; modfica allo schema org dell'index (aggiunta link alla licenza GPL). aggiunta cartella per contenere file node.js

--- asset/js/general.js
+++ asset/js/general.js
@@ -2,33 +2,31 @@ document.addEventListener("DOMContentLoaded", init);
 
 const token = "ae89f0d2ce4533d920af03eda0fa850b";
 
-function init() 
-{
+function init() {
     //init eventListener della darkmode
     document.getElementById("darkModeToggle").addEventListener("click", toggleDarkMode);
-    
+
     //init preferenza dark o light mode
     checkPref();
-    
+
     //init meteo città corrente
     getCurrentCityWeather();
 }
 
-// #region meteo
+// #region condizioni meteo in tempo reale
 
-async function getCurrentCityWeather()
-{
+async function getCurrentCityWeather() {
     navigator.geolocation.getCurrentPosition(async (position) => {
         let weather, weatherJson;
 
-        weather = await fetch("https://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&appid="+token+"&units=metric&lang=it",{method:"GET"});
+        weather = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=" + token + "&units=metric&lang=it", { method: "GET" });
         weatherJson = await weather.json();
 
         document.getElementById("currentCity").innerText = weatherJson.name;
-        document.getElementById("currentCityTemp").innerText = "temperatura: "+weatherJson.main.temp+"°";
-        document.getElementById("currentCityTempPerc").innerText = "temperatura percepita: "+weatherJson.main.feels_like+"°";
-        document.getElementById("currentCityHumid").innerText = "umidità: "+weatherJson.main.humidity+"%";
-    }); 
+        document.getElementById("currentCityTemp").innerText = "temperatura: " + weatherJson.main.temp + "°";
+        document.getElementById("currentCityTempPerc").innerText = "temperatura percepita: " + weatherJson.main.feels_like + "°";
+        document.getElementById("currentCityHumid").innerText = "umidità: " + weatherJson.main.humidity + "%";
+    });
 }
 
 /* vedere se tenere o meno
@@ -45,28 +43,24 @@ async function getCityCoord(city)
 
 // #region darkmode toggle
 
-function toggleDarkMode(){
-    if(document.body.classList.contains("bootstrap"))
-        {
-            document.body.className = "bootstrap-dark";
-            document.getElementById("topNavBar").className = "navbar navbar-expand-lg navbar bg-dark fixed-top";
-        }
-        else
-        {
-            document.body.className = "bootstrap";
-            document.getElementById("topNavBar").className = "navbar navbar-expand-lg navbar bg-light fixed-top";
-        }
+function toggleDarkMode() {
+    if (document.body.classList.contains("bootstrap")) {
+        document.body.className = "bootstrap-dark";
+        document.getElementById("topNavBar").className = "navbar navbar-expand-lg navbar bg-dark fixed-top";
+    }
+    else {
+        document.body.className = "bootstrap";
+        document.getElementById("topNavBar").className = "navbar navbar-expand-lg navbar bg-light fixed-top";
+    }
 
-        savePref();
+    savePref();
 }
 
-function checkPref()
-{
+function checkPref() {
     let pref = JSON.parse(localStorage.getItem("darkPref"));
     let date = new Date();
 
-    if(pref !== 'undefined' && pref.dark && (date.getTime() - pref.timestamp) < (1000*60*60*24*7))
-    {
+    if (pref !== 'undefined' && pref.dark && (date.getTime() - pref.timestamp) < (1000 * 60 * 60 * 24 * 7)) {
         document.body.className = "bootstrap-dark";
         document.getElementById("topNavBar").className = "navbar navbar-expand-lg navbar bg-dark fixed-top";
     }
@@ -75,17 +69,16 @@ function checkPref()
 
 }
 
-function savePref()
-{
+function savePref() {
     let timestamp = new Date();
     let pref = {};
 
-    if(document.body.className == "bootstrap")
+    if (document.body.className == "bootstrap")
         pref = {
             "dark": false,
             "timestamp": timestamp.getTime()
         };
-    else 
+    else
         pref = {
             "dark": true,
             "timestamp": timestamp.getTime()
@@ -93,7 +86,7 @@ function savePref()
 
     pref = JSON.stringify(pref);
 
-    localStorage.setItem("darkPref",pref);
+    localStorage.setItem("darkPref", pref);
 }
 
 // #endregion
