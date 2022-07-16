7bee0d3abf703f67ddda9de218618a5a318f59af
Teo <matteo.celardo.social@gmail.com>
2022-07-16 19:12:47 +0200

    separazione della funzione di toggle darkmode in un altro file js. creazione di pagine ejs per: login, registrazione, previsioni regionali e nazionali (solo login parzialmente scritta, le altre sono vuote). aggiornamenti dei percorsi alle pagine aggiunte tramite navbar dell'index. aggiornamento dei percorsi per renderizzare le pagine server aggiunti sul server

---
+++ assets/js/darkMode.js
@@ -0,0 +1,55 @@
+init();
+
+function init() {
+    //init eventListener della darkmode
+    document.getElementById("darkModeToggle").addEventListener("click", toggleDarkMode);
+
+    //init preferenza dark o light mode
+    checkPref();
+}
+
+function toggleDarkMode() {
+    if (document.body.classList.contains("bootstrap")) {
+        document.body.className = "bootstrap-dark";
+        document.getElementById("topNavBar").className = "navbar navbar-expand-lg navbar bg-dark fixed-top";
+    }
+    else {
+        document.body.className = "bootstrap";
+        document.getElementById("topNavBar").className = "navbar navbar-expand-lg navbar bg-light fixed-top";
+    }
+
+    savePref();
+}
+
+function checkPref() {
+    let pref = JSON.parse(localStorage.getItem("darkPref"));
+    let date = new Date();
+
+    if (pref !== 'undefined' && pref.dark && (date.getTime() - pref.timestamp) < (1000 * 60 * 60 * 24 * 7)) {
+        document.body.className = "bootstrap-dark";
+        document.getElementById("topNavBar").className = "navbar navbar-expand-lg navbar bg-dark fixed-top";
+    }
+
+    savePref();
+
+}
+
+function savePref() {
+    let timestamp = new Date();
+    let pref = {};
+
+    if (document.body.className == "bootstrap")
+        pref = {
+            "dark": false,
+            "timestamp": timestamp.getTime()
+        };
+    else
+        pref = {
+            "dark": true,
+            "timestamp": timestamp.getTime()
+        };
+
+    pref = JSON.stringify(pref);
+
+    localStorage.setItem("darkPref", pref);
+}
--- assets/node/server.js
+++ assets/node/server.js
@@ -1,30 +1,47 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-
 require("dotenv").config();
 
 const app = express();
 
+// #region impostazione dei percorsi standard per reperire le risorse
 app.use(express.static(path.join(__dirname,"../")));
 app.use("/css",express.static(path.join(__dirname,"../css")));
 app.use("/js",express.static(path.join(__dirname,"../js")));
 
 app.set("views", path.join(__dirname, "../../views"));
 app.set("view engine","ejs"); //motore ejs
+// #endregion
 
-app.use(session({
+/*app.use(session({
 	secret: process.env.SECRET_KEY,
     httpOnly: true,
 	resave: false,
 	saveUninitialized: false, 
 	cookie: {secure: false, maxAge: 15*60*1000}
-}));
+}));*/
 
 // #region routing
 
 app.get("/",(req,res) => {
-    res.render("index");
+    res.render("index").status(200).end();
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login/login").status(200).end();
+});
+
+app.get("/registrazione",(req,res)=>{
+    res.render("login/registrazione").status(200).end();
+});
+
+app.get("/previsioniRegionali", (res,req)=>{
+    res.render("previsioni/regionali").status(200).end();
+});
+
+app.get("/previsioniNazionali", (res,req)=>{
+    res.render("previsioni/nazionali").status(200).end();
 });
 
 // #endregion
