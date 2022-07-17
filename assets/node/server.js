229991b4b12d48db5d6a43510a9cf533e86d7c37
Teo <matteo.celardo.social@gmail.com>
2022-07-17 13:34:33 +0200

    funzione di login correttamente funzionante (ancora da sistemare la ricezione del cookie nell'index)

--- assets/node/server.js
+++ assets/node/server.js
@@ -13,15 +13,16 @@ const listenPort = process.env.PORT || 3000;
 const DBName = process.env.DB_NAME;
 const mc = new MongoClient(process.env.DB_URL);
 const app = express();
+const expireTime = 15 * 60 * 1000;
 // #endregion
 
 // #region impostazioni middleware
-app.use(express.static(path.join(__dirname,"../")));
-app.use("/css",express.static(path.join(__dirname,"../css")));
-app.use("/js",express.static(path.join(__dirname,"../js")));
+app.use(express.static(path.join(__dirname, "../")));
+app.use("/css", express.static(path.join(__dirname, "../css")));
+app.use("/js", express.static(path.join(__dirname, "../js")));
 
 app.set("views", path.join(__dirname, "../../views"));
-app.set("view engine","ejs"); //motore ejs
+app.set("view engine", "ejs"); //motore ejs
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -31,39 +32,50 @@ app.use(cookie());
 
 // #region routing
 
-app.get("/",(req,res) => {
-    if(req.cookies.nickName !== undefined)
-        res.render("index",{autenticato:true}).status(200).end();
+app.get("/", (req, res) => {
+    if (req.cookies.nickName !== undefined)
+        res.render("index", { autenticato: true, user: req.cookies.nickName }).status(200).end();
     else
-        res.render("index",{autenticato:false,user:req.cookies.nickName}).status(200).end();
+        res.render("index", { autenticato: false }).status(200).end();
 });
 
-app.get("/login",(req,res)=>{
-    res.render("login/login").status(200).end();
+app.get("/login", (req, res) => {
+    let auth = req.query.auth ? true : false;
+
+    res.render("login/login", { credenzialiErrate: auth }).status(200).end();
 });
 
-app.get("/registrazione",(req,res)=>{
+app.get("/registrazione", (req, res) => {
     res.render("login/registrazione").status(200).end();
 });
 
-app.get("/previsioniRegionali", (req,res)=>{
+app.get("/previsioniRegionali", (req, res) => {
     res.render("previsioni/regionali").status(200).end();
 });
 
-app.get("/previsioniNazionali", (req,res)=>{
+app.get("/previsioniNazionali", (req, res) => {
     res.render("previsioni/nazionali").status(200).end();
 });
 
 app.get("/verificaCredenziali", (req, res) => {
-    res.send(req.query);
-    mc.connect(function(err, db) {
+    let {mail,pw} = req.query;
+    mc.connect(function (err, db) {
         if (err) throw err;
         let dbo = db.db(DBName);
-        let query = { mail: req.query.email };
-        let result = dbo.collection("users").find(query);
-        //if(result !== undefined)
-          //  res.
-      });
+        let query = { email: mail };
+        dbo.collection("users").findOne(query, function (err, result) {
+            if (err) throw err;
+            if (result !== null && result.pw == pw)
+                res.cookie("login", result.user, {
+                    maxAge: expireTime,
+                    httpOnly: true
+                }).redirect("/");
+            else
+                res.redirect("/login?auth=fail");
+            db.close();
+        });
+
+    });
 });
 
 app.post("/creaUtente", (req, res) => {
@@ -71,21 +83,8 @@ app.post("/creaUtente", (req, res) => {
 });
 // #endregion
 
-const server = app.listen(listenPort, ()=>{
-	
+const server = app.listen(listenPort, () => {
 
-	console.log(`Applicazione in ascolto su porta ${listenPort}`);
+
+    console.log(`Applicazione in ascolto su porta ${listenPort}`);
 });
-
-/*
-per inserire cookie
-
-res.cookie(`Cookie token name`,`encrypted cookie string Value`,{
-        maxAge: 5000,
-        // expires works the same as the maxAge
-        expires: new Date('01 12 2021'),
-        secure: true,
-        httpOnly: true,
-        sameSite: 'lax'
-    });
-*/
